23ec0811bd119c36e1222bfb058a9fc9d34ee201
Gladys Alvarez <glad.alvarez1@gmail.com>
2020-02-29 16:46:12 -0600

    Jasmine test examples

---
+++ spec/my-jasmine.test.js
@@ -0,0 +1,15 @@
+describe ("sayHi function", () => {
+  it ("should return Hola", () => {
+    expect(sayHi()).toEqual("Hola");
+  });
+});
+
+describe("Mi calcu", () => {
+  it("should return 0 if the array is empty", () => {
+    expect(sumArray([])).toEqual(0);
+  });
+
+  it("should return the number if the array only has one value", () => {
+    expect(sumArray([3])).toEqual(3)
+  });
+});
